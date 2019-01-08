908f18be88a229c57d494780621efe8f1b2aabfa
samdlcong <samnew@126.com>
2019-01-08 11:34:57 +0800

    add vue-bookstore

--- vue-todos/src/App.vue
+++ vue-todos/src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
     <h1>{{title}}</h1>
-    <ul v-for="(todo,index) in todos" :id="index" class="todos">
-      <li><label for="">{{index+1}}.{{todo.value}}</label></li>
+    <ul v-for="(todo,index) in todos" :id="index" class="todos" >
+      <li :class="{'checked': todo.done}">
+        <label for="">{{index+1}}.{{todo.value}}</label>
+        <time>{{ todo.created| date }}</time>
+      </li>
     </ul>
   </div>
 </template>
@@ -13,6 +16,9 @@
 <script>
 import './assets/todos.less';
 import './assets/site.less';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+moment.locale('zh-cn');
 
 export default {
   name:'app',
@@ -20,11 +26,16 @@ export default {
     return{
       title:"vue-todos",
       todos:[
-        {value:"阅读一本书",done:false},
-        {value:"补充一下什么", done:true},
-        {value:"写心得", done: false}
+        {value:"阅读一本书",done:false, created:Date.now()},
+        {value:"补充一下什么", done:true, created:Date.now()+30000000},
+        {value:"写心得", done: false, created:Date.now()-30000000}
       ]
     }  
+  },
+  filters: {
+    date(val){
+      return moment(val).calendar();
+    }
   }
 }
 </script>
