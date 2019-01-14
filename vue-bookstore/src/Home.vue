0f1375f4ceb814b490eab11d2e7e59c28388ab9d
samdlcong <samnew@126.com>
2019-01-14 11:11:38 +0800

    swiper

--- vue-bookstore/src/Home.vue
+++ vue-bookstore/src/Home.vue
@@ -2,21 +2,22 @@
 <template>
     <div>
         <div class="section">
-            <div class="swiper-container" rel="slider">
+            <div class="swiper-container" ref="slider">
                 <!--热门推荐 -->
-                <div class="swiper-wrapper">
-                    <div class="swiper-slider">
-                        <img src="./fixtures/sliders/t1.svg" alt="">
+                <div class="swiper-wrapper" v-for="slide in slides" :key='slide.id'>
+                    <div class="swiper-slider" >
+                        <img :src="slide.img_url" alt="">
                     </div>
                 </div>
-                <div class="swiper-wrapper">
+                <!-- <div class="swiper-wrapper">
                     <div class="swiper-slider">
                         <img src="./fixtures/sliders/t2.svg" alt="">
                     </div>
-                </div>
+                </div> -->
                 <div class="swiper-pagination" ref="pagination">
-                    
+
                 </div>
+               
                 <!-- 快讯 -->
             </div>
         </div>
@@ -33,10 +34,39 @@
 </style>
 <script>
 import Swiper from "swiper"
-import 'swiper/dist/css/swiper.css'
+// import '/static/swiper/dist/css/swiper.css'
 export default {
-    
+    data(){
+        return {
+            slides:[
+                {id:1, img_url:'/static/image/t1.svg'},
+                {id:2, img_url:'/static/image/t2.svg'}
+            ]
+        }
+    },
+    mounted() {
+        // new Swiper(this.$refs.slider, {
+        //     pagination: this.$refs.pagination,
+        //     paginationClickable: true,
+        //     spaceBetween: 30,
+        //     centeredSlides:true,
+        //     autoplay: 2500,
+        //     autoplayDisableOnInteraction: false
+        // })
+        new Swiper(this.$refs.slider, {
+                pagination: this.$refs.pagination,
+                paginationClickable: true,
+                spaceBetween: 30,
+                centeredSlides: true,
+                autoplay: 2500,
+                autoplayDisableOnInteraction: false
+            })
+    }
 }
 </script>
+<style>
+/* @import '/static/swiper/dist/css/swiper.css'; */
+
+</style>
 
 
