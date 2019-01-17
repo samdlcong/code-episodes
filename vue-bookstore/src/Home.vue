389dbf12835bddc2cdc3d72c7b8a2509224325a6
samdlcong <samnew@126.com>
2019-01-17 14:57:44 +0800

    announcement模块

--- vue-bookstore/src/Home.vue
+++ vue-bookstore/src/Home.vue
@@ -13,127 +13,32 @@
                 
             </div>
             <!-- 快讯 -->
-            <div class="announcement">
+            <!-- <div class="announcement">
                 <label for="">快讯</label>
                 <span>{{ announcement }}</span>
-            </div>
+            </div> -->
+            <announcement :announcement="announcement" label="快讯"></announcement>
         </div>
         <div class="section">
             <!-- 新书上架 -->
             <book-list :books="latestUpdated" heading="最新更新">
                 
             </book-list>
-            <!-- <div class="book-list">
-                <div class="header">
-                    <div class="heading">最新更新</div>
-                    <div class="more">更多...</div>
-                </div>
-                <div class="book-items">
-                    <div class="book" v-for="book in latestUpdated">
-                        <div class="cover"><img :src="book.img_url|fullUrl" alt=""></div>
-                        <div class="title">{{book.title}}</div>
-                        <div class="authors">{{book.authors|join}}</div>
-                    </div> -->
-                    <!-- <div class="book">
-                        <div class="cover"><img src="/static/covers/2.svg" alt=""></div>
-                        <div class="title">Android 高级进阶</div>
-                        <div class="authors">顾浩鑫</div>
-                    </div>
-                    <div class="book">
-                        <div class="cover"><img src="/static/covers/3.svg" alt=""></div>
-                        <div class="title">淘宝天猫电商运营与数据化选品完全手册</div>
-                        <div class="authors">老夏</div>
-                    </div>
-                    <div class="book">
-                        <div class="cover"><img src="/static/covers/4.svg" alt=""></div>
-                        <div class="title">大数据架构详解：从数据获取到深度学习</div>
-                        <div class="authors">朱洁,罗华霖</div>
-                    </div>
-                    <div class="book">
-                        <div class="cover"><img src="/static/covers/5.svg" alt=""></div>
-                        <div class="title">Meteor 全栈开发</div>
-                        <div class="authors">杜亦舒</div>
-                    </div>
-                    <div class="book">
-                        <div class="cover"><img src="/static/covers/6.svg" alt=""></div>
-                        <div class="title">Kubernetes 权威指南：从 Docker 到 KUbernetes 实践全接触（第2版）</div>
-                        <div class="authors">龚正,吴志辉,王伟,崔秀龙,闫键勇</div>
-                    </div> -->
-                <!-- </div>
-            </div> -->
         </div>
         <div class="section">
             <!-- 编辑推荐 -->
             <book-list :books="recommended" heading="编辑推荐">
 
             </book-list>
-            <!-- <div class="book-list">
-                <div class="header">
-                    <div class="heading">编辑推荐</div>
-                    <div class="more">更多...</div>
-                </div>
-                <div class="book-items">
-                    <div class="book" v-for="book in recommended">
-                        <div class="cover"><img :src="book.img_url|fullUrl" alt=""></div>
-                        <div class="title">{{book.title}}</div>
-                        <div class="authors">{{book.authors|join}}</div>
-                    </div> -->
-                    <!-- <div class="book">
-                        <div class="cover"><img src="/static/covers/2.svg" alt=""></div>
-                        <div class="title">智能硬件安全</div>
-                        <div class="authors">刘健皓</div>
-                    </div>
-                    <div class="book">
-                        <div class="cover"><img src="/static/covers/3.svg" alt=""></div>
-                        <div class="title">实战数据库营销——大数据时代轻松赚钱之道</div>
-                        <div class="authors">罗安林</div>
-                    </div>
-                    <div class="book">
-                        <div class="cover"><img src="/static/covers/4.svg" alt=""></div>
-                        <div class="title">大数据架构详解：从数据获取到深度学习</div>
-                        <div class="authors">朱洁,罗华霖</div>
-                    </div>
-                    <div class="book">
-                        <div class="cover"><img src="/static/covers/5.svg" alt=""></div>
-                        <div class="title">Meteor 全栈开发</div>
-                        <div class="authors">杜亦舒</div>
-                    </div>
-                    <div class="book">
-                        <div class="cover"><img src="/static/covers/6.svg" alt=""></div>
-                        <div class="title">Kubernetes 权威指南：从 Docker 到 KUbernetes 实践全接触（第2版）</div>
-                        <div class="authors">龚正,吴志辉,王伟,崔秀龙,闫键勇</div>
-                    </div> -->
-                <!-- </div>
-            </div> -->
+           
         </div>
     </div>
 </template>
-<style lang="less">
-    .announcement {
-        font-size: 12px;
-        padding:5px;
-        background:#fff;
-    }
 
-    .announcement > label {
-        padding: 3px 10px;
-        margin: 3px 10px;
-        background: #FACCB7;
-        border-radius: 3px;
-        color: #FF5102;
-
-    }
-
-    .announcement > span {
-        padding: 3px 10px;
-        color: #333;
-        display:inline-block;
-    }    
-
-</style>
 <script>
 import Swiper from "swiper"
 import BookList from "./components/BookList.vue"
+import Announcement from "./components/Announcement.vue"
 import 'swiper/dist/css/swiper.css'
 export default {
     data(){
@@ -257,7 +162,8 @@ export default {
         })
     },
     components:{
-        BookList
+        BookList,
+        Announcement
     }
     
 }
---
+++ vue-bookstore/src/components/Announcement.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="announcement">
+        <label for="">{{label}}</label>
+        <span>{{ announcement }}</span>
+    </div>
+</template>
+<style lang="less" scoped>
+    .announcement {
+        font-size: 12px;
+        padding:5px;
+        background:#fff;
+    }
+
+    .announcement > label {
+        padding: 3px 10px;
+        margin: 3px 10px;
+        background: #FACCB7;
+        border-radius: 3px;
+        color: #FF5102;
+
+    }
+
+    .announcement > span {
+        padding: 3px 10px;
+        color: #333;
+        display:inline-block;
+    }    
+</style>
+<script>
+export default {
+    props:[
+        'announcement',
+        'label'
+    ]
+}
+</script>
+
