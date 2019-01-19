6bc9bc6a2b9d1027675000b2db16df1e315b796a
samdlcong <samnew@126.com>
2019-01-19 16:56:55 +0800

    add uikit

---
+++ vue-curd/src/uikit.js
@@ -0,0 +1,15 @@
+import 'jquery'
+import 'uikit'
+import 'uikit-css'
+
+export default (Vue, options) =>{
+    const ui = window.UIkit
+    Vue.prototype.$ui = {
+        $: ui.$,
+        modal: ui.modal,
+        alert: ui.modal.alert,
+        confirm: ui.modal.confirm,
+        prompt: ui.modal.prompt,
+        block: ui.modal.blockUI
+    }
+}
